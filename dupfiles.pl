362c6c1f15b9997bc4c326b7431c04d59d586ae1
Donald R. Kasper <donald.kasper@gmail.com>
2014-05-28 01:29:56 -0400

    another patch to dupfiles

--- dupfiles.pl
+++ dupfiles.pl
@@ -1,45 +1,45 @@
 #!/usr/bin/perl
-
+ 
 # use strict;
 use warnings;
 use POSIX;
 use Digest::MD5;
-
+ 
 ## Setting Initial Directory to check
-
-$result_file = "/tmp/dupfiles.dump";
-
+ 
+my $result_file = "/tmp/dupfiles.dump";
+ 
 my $Location;
-
-if (!$ARGV[0]) 
+ 
+if (!$ARGV[0])
 {
-	print "Location to Search? ";
-	$Location = <STDIN>; 
-	chomp($Location);	
+        print "Location to Search? ";
+        $Location = <STDIN>;
+        chomp($Location);      
 }
-else 
-{	
-	$Location = $ARGV[0];	
+else
+{      
+        $Location = $ARGV[0];  
 }
-
+ 
 # copy/paste from
 # http://rinkeshbansal.blogspot.com/2012/08/calculating-md5dum-of-all-files-in.html
 sub md5sum_dir($)
 {
     my $path = shift;
-
+ 
     opendir (DIR, $path) or die "Unable to open $path: $!";
     my @files = map {$path . '/' . $_ }grep { !/^\.{1,2}$/ } readdir (DIR);
     closedir (DIR);
-
-    foreach my $file (@files) 
+ 
+    foreach my $file (@files)
     {
-        if (-d $file) 
+        if (-d $file)
         {
             &md5sum_dir ($file);
-        } 
-        else 
-        { 
+        }
+        else
+        {
             my $md = &md5sum($file); # you can find this subroutine in my other blogs
             open (FO, ">>$result_file") or die ("Error :: Couldn't open file ($result_file) for writing\n");
             print FO "$file\t$md\n";
@@ -47,24 +47,24 @@ sub md5sum_dir($)
         }
     }
 }
-
+ 
 # copy/paste from
 # http://rinkeshbansal.blogspot.in/2012/08/calculating-md5sum-of-given-file.html
 sub md5sum($)
 {
     my $file = shift;
     die "Error::File ($file) does not exist!!!"    if (not -e $file);
-    open (FO, $file);
-    binmode(FO);
+    open (FI, $file);
+    binmode(FI);
     my $md5 = Digest::MD5->new;
-    while ($_) 
-    {
-        $md5->add($_);
-    }
-    close(FO);
+#    while ($_)
+#    {
+        $md5->addfile(*FI);
+#    }
+    close(FI);
     my $checksum = $md5->hexdigest;
     print "$checksum $file\n";
     return $checksum;
 }
-
+ 
 &md5sum_dir($Location);
