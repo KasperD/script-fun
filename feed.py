cfe69d30897bea3310bc0c6186a30145b5f518d3
Donald R. Kasper <donald.kasper@gmail.com>
2015-07-13 21:18:00 -0400

    not sure why this works

--- feed.py
+++ feed.py
@@ -1,16 +1,20 @@
 import feedparser
 import datetime
+import time
 
 d = feedparser.parse("http://blogsuki.com/feed/")
+# d = feedparser.parse("http://www.techmeme.com/feed.xml")
+doy = datetime.datetime.now().strftime('%j')
+print doy
 
 for post in d.entries:
-	print post.title
-	print post.link
-	print post.published 
-	print post.published_parsed
-
-today = datetime.datetime.now()
-day_of_year = today.strftime('%j')
-print day_of_year
+	# print post.title
+	# print post.link
+	# print post.published
+        # print post.published_parsed[7]
+    if  (int(doy) - post.published_parsed[7]) <= 14:
+        print post.title
+        print post.link
+        print post.published_parsed[:9]
 print d.version
 print "end"
