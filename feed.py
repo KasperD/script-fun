e3f4d730c9ab4b5afe9c66221c4432061a789fd6
Donald R. Kasper <donald.kasper@gmail.com>
2015-07-16 11:01:19 -0400

    pewp

--- feed.py
+++ feed.py
@@ -2,19 +2,22 @@ import feedparser
 import datetime
 import time
 
-d = feedparser.parse("http://blogsuki.com/feed/")
-# d = feedparser.parse("http://www.techmeme.com/feed.xml")
+# d = feedparser.parse("http://blogsuki.com/feed/")
+d = feedparser.parse("http://www.techmeme.com/feed.xml")
 doy = datetime.datetime.now().strftime('%j')
 print doy
+updates = []
 
 for post in d.entries:
-	# print post.title
-	# print post.link
-	# print post.published
-        # print post.published_parsed[7]
     if  (int(doy) - post.published_parsed[7]) <= 14:
+        duckie_date = ""
         print post.title
         print post.link
         print post.published_parsed[:9]
-print d.version
-print "end"
+        for elements in post.published_parsed[:]:
+            if elements < 10:
+                duckie_date = duckie_date + "0"
+            duckie_date = duckie_date + str(elements)
+        updates.append([post.title[:0], post.link[:0], duckie_date])
+print "*" * 80
+print updates
