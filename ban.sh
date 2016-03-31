e25e59f690fc9951f831680cb9ffc1be25f86fc9
Donald Kasper <donald.kasper@gmail.com>
2016-03-31 18:20:49 +0000

    oops, forgot to delete files

--- ban.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo -n "IP Address: "
-read ipaddr
-/sbin/iptables -A assholes -s $ipaddr -j DROP
-/sbin/iptables -A assholes -d $ipaddr -j DROP
-echo "added ip address"
--- file-create.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-for each in range(1000):
-    FileName = str(each) + " - filename" + str(each) + ".txt"
-    print FileName
-print "Done..."
