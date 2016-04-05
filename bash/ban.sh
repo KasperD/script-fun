586740a41018a13bc3e9f9d0f25a1118cc6caaef
U-CROSSVIEW\dkasper <dkasper@R9027770.crossview.inc>
2016-04-05 14:04:48 -0400

    root only for ban script

--- bash/ban.sh
+++ bash/ban.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+if [ $UID -ne 0 ]; then
+    echo "Must be root"
+    exit 1
+fi
+
 echo -n "IP Address: "
 read ipaddr
 /sbin/iptables -A assholes -s $ipaddr -j DROP
 /sbin/iptables -A assholes -d $ipaddr -j DROP
 echo "added ip address"
+   
