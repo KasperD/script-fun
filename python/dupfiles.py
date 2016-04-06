84a582db1a7f7889b8c0180cb6d61125633b3e5f
Donald Kasper <donald.kasper@gmail.com>
2016-04-06 11:30:26 -0400

    name changes and a duplicate removal

---
+++ python/dupfiles.py
@@ -0,0 +1,14 @@
+#!/bin/python
+'''
+================================================================================
+Script Purpose: The purpose of this script is to recursively search a directory
+for files matching an extension. Then run a hash against the file in order to
+determine if the file is a duplicate of the other under a different name.
+================================================================================
+'''
+
+if __name__ == '__main__':
+    print "Good"
+
+else:
+    print "Bad"
