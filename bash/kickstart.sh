97fb73e8b51fa971636adf8405bb964fa1671289
Donald R. Kasper <donald.kasper@gmail.com>
2017-11-17 08:45:02 -0500

    adding apt and pip requierment files

--- bash/kickstart.sh
+++ bash/kickstart.sh
@@ -1,4 +1,30 @@
 #!/bin/bash
 
-echo "Debian / Ubuntu apt-get update"
+# Check for root access
+if [ $UID -ne 0 ]; then
+    echo "Must be root"
+    exit 1
+fi
 
+PIPFILE="./requirements.txt"
+APTFILE="./aptreq.txt"
+
+OracleInstall() {
+    add-apt-repository ppa:webupd8team/java
+    apt-get install oracle-java9-installer
+}
+
+AptUpgrade() {
+    echo "apt-get update & upgrade"
+    apt-get update
+    apt-get upgrade -y
+    apt-get autoremove -y
+    apt-get clean
+}
+
+PipUpgrade() {
+    pip install -r $PIPFILE
+}
+
+AptUpgrade
+PipUpgrade
